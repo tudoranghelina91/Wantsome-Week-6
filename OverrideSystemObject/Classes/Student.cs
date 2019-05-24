a65733d5015fe2e41676b646682c2aaa87ba9707
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-24 19:36:56 +0300

    Updated class project

---
+++ OverrideSystemObject/Classes/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideSystemObject.Classes
+{
+    class Student
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this.Name == (obj as Student).Name && this.Age == (obj as Student).Age)
+                return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}\n");
+            sb.Append($"Age: {Age}\n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ OverrideSystemObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OverrideSystemObject.Classes;
+
+namespace OverrideSystemObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Marian Vasile", 26);
+            Student student2 = new Student("Gigi Muschi", 26);
+            Student student3 = new Student("Marian Vasile", 26);
+
+            Console.WriteLine(student.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.WriteLine(student3.ToString());
+
+            Console.WriteLine($"student 1 is equal to student 2? {student.Equals(student2)}");
+            Console.WriteLine($"student 2 is equal to student 3? {student.Equals(student3)}");
+
+            Console.ReadKey();
+        }
+    }
+}
